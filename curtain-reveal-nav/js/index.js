89cfe4762e84e8b2b3a08ac5183bffdd61783b2a
AjitMSH <shaw.ajit02@gmail.com>
2022-11-13 13:34:59 +0530

    Effect added

---
+++ curtain-reveal-nav/js/index.js
@@ -0,0 +1,42 @@
+// const left = document.getElementById('left_side')
+
+// const onHandleMove = e => {
+
+//     const p = e.clientX / window.innerWidth * 100
+
+//     left.style.width = `${p}%`
+// }
+
+// document.onmousemove = e => onHandleMove(e)
+
+// // For Mobiles
+// document.ontouchmove = e => onHandleMove(e.touches[0]);
+
+const navToggle = () => {
+  document.body.dataset.nav = document.body.dataset.nav === 'true' ? 'false' : 'true';
+}
+
+const imageChange = (e) => {
+  // e.preventDefault();
+  const src = e.dataset.src;
+  document.getElementById('main').getElementsByTagName('img')[0].src = src
+
+}
+
+// Slider
+
+// new Swiper('.logo_slider .slider', {
+//     direction: 'horizontal',
+//     loop: true,
+//     speed: 6000,
+//     freeMode: true,
+//     freeModeMomentum: false,
+//     freeModeMomentumBounce: false,
+//     grabCursor: false,
+//     slidesPerView: 'auto',
+//     allowTouchMove: false,
+//     autoplay: {
+//       delay: 1,
+//       disableOnInteraction: false,
+//     }
+// });
